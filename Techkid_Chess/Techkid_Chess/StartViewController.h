d44b2e1a00f80e8f66591316b21219bd24589e73
vinhnguyendinh <vinhnguyendinhbg@gmail.com>
2016-06-05 11:47:21 +0700

    update

---
+++ Techkid_Chess/Techkid_Chess/StartViewController.h
@@ -0,0 +1,13 @@
+//
+//  StartViewController.h
+//  Techkid_Chess
+//
+//  Created by Vinh Nguyen Dinh on 6/5/16.
+//  Copyright © 2016 TechKid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StartViewController : UIViewController
+
+@end
